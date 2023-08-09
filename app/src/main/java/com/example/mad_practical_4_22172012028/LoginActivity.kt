6e825989a878bcbf3eab72a3db9a8d2c578e0da7
student <student@gnu.ac.in>
2023-08-09 08:56:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_4_22172012028/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mad_practical_4_22172012028
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+}
---
+++ app/src/main/java/com/example/mad_practical_4_22172012028/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mad_practical_4_22172012028
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    val tag:Button = findViewById(R.id.button)
+}
