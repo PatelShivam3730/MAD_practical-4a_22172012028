b1ef55dae2ed7effaaed4ae50738adc8fed4a1bf
student <student@gnu.ac.in>
2023-08-11 12:38:54 +0530

    p4

--- app/src/main/java/com/example/mad_practical_4_22172012028/activity_registration.kt
+++ app/src/main/java/com/example/mad_practical_4_22172012028/activity_registration.kt
@@ -1,17 +1,11 @@
 package com.example.mad_practical_4_22172012028
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
-class MainActivity : AppCompatActivity()
-{
+class activity_registration : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val tag:Button = findViewById(R.id.button)
+        setContentView(R.layout.activity_registration)
     }
-
-
 }
